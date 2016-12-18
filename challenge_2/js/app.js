9c2f170acbeb8917cae5110680e36d70e3a5eee0
git-clay <ce.smith5525@gmail.com>
2016-12-18 14:56:22 -0700

    #3 console logs interest but will not append to page

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,66 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("ClassController", ClassController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+  this.full_name = "Clay Smith";
+  this.age = '25';
+  this.city = 'Kernersville';
+  this.state = 'NC';
+
+}
+app.filter('shout', function(){
+	return function(input){
+		input = input || '';
+		var out = input +'!';
+		return out;
+	};
+});
+
+function ClassController($scope){
+	this.class_name= "Big Nick's Cult of Zebulon";
+	$scope.enrolled_students=[
+	{
+		lastName:'Ridge',
+		firstName:'Brian'
+	},	{
+		lastName:'Issac',
+		firstName:'Erik'
+	},	{
+		lastName:'Walter',
+		firstName:'Gisella'
+	},	{
+		lastName:'Liechty',
+		firstName:'Guy'
+	},	{
+		lastName:'Bloomquist',
+		firstName:'Kevin'
+	},	{
+		lastName:'Kraus',
+		firstName:'Larry'
+	},	{
+		lastName:'Schurr',
+		firstName:'Merry'
+	},	{
+		lastName:'Leibowitz',
+		firstName:'Nate'
+	},	{
+		lastName:'Wierenga',
+		firstName:'Micha'
+	},	{
+		lastName:'Smith',
+		firstName:'Clay'
+	}
+	];
+	$scope.startDate = new Date(10,31,2016)
+	$scope.endDate ='02/02/2017';
+	$scope.daysRemaining = function(){
+
+	};
+}
+
+
+
+
--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,36 @@
 console.log('challenge #3 -- app.js loaded!');
 
-// var app = angular.module("appThree", []);
-// app.controller("infoController", infoController)
+var app = angular.module("appThree", []);
+app.controller("infoController", infoController);
 
-// function infoController(){
-//   this.name = "Jon Doe"; // default name
-//   this.height = 5.6; // default height in feet
-// }
+function infoController(){
+  this.name = "Jon Doe"; // default name
+  this.height = 5.6; // default height in feet
+
+}
+
+
+
+function loanController($scope){
+	this.amount = 0;
+	this.apr = 0;
+	this.month = function(val){
+		$scope.accrued_interest= accrued_interest(val,this.amount,this.apr);
+		this.total_balance=total_balance(this.accrued_interest);
+	};
+
+
+}
+
+function accrued_interest(val,amount,apr){
+	var interest = val*amount*(apr/100);
+	console.log(interest,'interest');
+	return interest;
+}
+function total_balance(stuff){
+console.log(stuff)
+	// var interest = val*amount*apr;
+	// console.log("balance")
+	// return interest;
+}
+app.controller("loanController", loanController);
